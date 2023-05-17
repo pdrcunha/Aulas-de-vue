d320443e12aa702e836fc3d190723f1c359f3ee5
Pedro Cunha <pedro.2112@hotmail.com.br>
2023-05-17 20:54:47 -0300

    Finish modulo 1

---
+++ Projeto sem rota/src/App.vue
@@ -0,0 +1,29 @@
+<script >
+import FirstComponent from './components/FirstComponent.vue';
+
+export default {
+  data() {
+    return {
+     
+    };
+  },
+  methods: {
+    
+  },
+  components:{
+    FirstComponent
+}
+};
+</script>
+
+<template>
+  <div>
+   <FirstComponent/>
+  </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
---
+++ Projeto sem rota/src/components/FirstComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        Meu Primeiro Componente
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+     
+    };
+  },
+  methods: {
+    
+  },
+
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
